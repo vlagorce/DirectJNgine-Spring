9089e0196d613a0bb34d77a47ba73629c7da3bb2
vlagorce <vlagorce@gmail.com>
2010-04-23 15:03:44 +0200

    First import

---
+++ maven-install/directjngine/1.3-apha1/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+#directjngine.1.3.alpha1.jar
+mvn install:install-file -DgroupId=com.softwarementors.extjs -DartifactId=djn -Dversion=1.3.alpha1 -Dpackaging=jar -Dfile=directjngine.1.3.alpha1.jar -DpomFile=pom.xml
+
+#directjngine.1.3.alpha1.javadoc.jar
+mvn install:install-file -DgroupId=com.softwarementors.extjs -DartifactId=djn -Dversion=1.3.alpha1 -Dpackaging=jar -Dclassifier=javadoc -Dfile=directjngine.1.3.alpha1.javadoc.jar
+  
+#directjngine.1.3.alpha1.src.jar
+mvn install:install-file -DgroupId=com.softwarementors.extjs -DartifactId=djn -Dversion=1.3.alpha1 -Dpackaging=jar -Dclassifier=sources -Dfile=directjngine.1.3.alpha1.src.jar
+
+
